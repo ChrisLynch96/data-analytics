2a415c9a625d77f155da20e31540b1ddc085cd63
ChrisLynch96 <chlynch@tcd.ie>
2019-10-16 10:50:06 +0100

    Files for lab 6 and 7

--- labs/lab6/lab6.R
+++ labs/lab6/lab6.R
@@ -7,3 +7,12 @@ str(Data)
 attach(Data)
 head(Data)
 summary(Data)
+
+Impute1 <- kNN(Data, variable= "Genre", k=5)
+summary(Impute1)
+
+Impute2 <- kNN(Data, variable= c("Audience..score..", "Profitability"), k=6)
+summary(Impute2)
+
+Impute3 <- kNN(Data, variable= "Rotten.Tomatoes..", k=6)
+summary(Impute3)
