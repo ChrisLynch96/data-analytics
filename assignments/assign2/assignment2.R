dfba1b23b79136b852ff1789aedc86c031e1bb8d
ChrisLynch96 <chlynch@tcd.ie>
2019-10-29 19:54:10 +0000

    final code for assginemtn 2

--- assignments/assign2/assignment2.R
+++ assignments/assign2/assignment2.R
@@ -6,23 +6,32 @@ rm(list = ls())
 
 # functions
 
-get_average_min_xerror <- function(num_runs, formula, data) {
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+get_average <- function(num_runs, formula, data) {
   xerrors = rep(NA, num_runs)
+  cps = rep(NA, num_runs)
   
   for(i in 1:num_runs) {
-    model <- rpart(formula = formula, data, method = "class", control = rpart.control())
+    model <- rpart(formula = formula, data = data, method = "class")
     opt <- which.min(model$cptable[,"xerror"])
     xerrors[i] <- model$cptable[opt, "xerror"]
+    cps[i] <- model$cptable[opt, "CP"]
   }
+  result <- list(
+    mean(xerrors),
+    getmode(cps)
+    )
   
-  return(mean(xerrors))
+  return(result)
 }
 
 # main
 
 data <- read_excel("Project Data.xlsx")
-
-xerrors <- list()
 
 formulas <- list(
   Response~X1 + X2 + X3 + X4 + X5 + X6 + X7,
@@ -36,87 +45,91 @@ sub_sets <- list(
   group2 <- subset(data, group = 1)
 )
 
+xerrors <- list()
+cps <- list()
+
 count = 1
 for(i in seq(length(sub_sets))) {
   for(j in seq(length(formulas))) {
-    xerrors[count] <- get_average_min_xerror(100, formulas[[j]], sub_sets[[i]])
-    print("subset:")
-    print(str(sub_sets[[i]]))
-    print("formula:")
-    print(formulas[[j]])
-    print("xerror")
-    print(xerrors[count])
+    result <- get_average(200, formulas[[j]], sub_sets[[i]])
+    xerrors[count] <- result[[1]]
+    cps[count] <- result[[2]] 
     count = count + 1
   }
 }
 
 print(xerrors)
+print(cps)
 
-# garbage
+# Prune and print decision trees
 
-multiple_runs <- function(num_runs) {
-  cps <- list()
-  for(i in 1:num_runs) {
-    counter = 1
-    models <- list()
-    model1 <- rpart(Response~X1 + X2 + X3 + X4 + X5 + X6 + X7, data, method = "class", parms = list(split = "information"))
-    models[[counter]] = model1
-    cp <- get_optimal_cp(model1)
-    cps[[counter]] = cps[[counter]] + cp
-    counter = counter + 1
-    
-    
-    
-    model2 <- rpart(Response~Y1 + Y2 + Y3 + Y4 + Y5 + Y6 + Y7, data, method = "class", parms = list(split = "information"))
-    models[[counter]] = model2
-    counter = counter + 1
-    
-    model3 <- rpart(Response~X1 + X2 + X3 + X4 + X5 + X6 + X7 + Y1 + Y2 + Y3 + Y4 + Y5 + Y6 + Y7, data, method = "class", parms = list(split = "information"))
-    models[[counter]] = model3
-    counter = counter + 1
-    
-    # Group = 0
-    group0 <- subset(data, group = 0)
-    model4 <- rpart(Response~X1 + X2 + X3 + X4 + X5 + X6 + X7, group0, method = "class", parms = list(split = "information"))
-    models[[counter]] = model4
-    counter = counter + 1
-    
-    model5 <- rpart(Response~Y1 + Y2 + Y3 + Y4 + Y5 + Y6 + Y7, group0, method = "class", parms = list(split = "information"))
-    models[[counter]] = model5
-    counter = counter + 1
-    
-    model6 <- rpart(Response~X1 + X2 + X3 + X4 + X5 + X6 + X7 + Y1 + Y2 + Y3 + Y4 + Y5 + Y6 + Y7,group0, method = "class", parms = list(split = "information"))
-    models[[counter]] = model6
-    counter = counter + 1
-    
-    # Group = 1
-    group1 <- subset(data, group = 1)
-    model7 <- rpart(Response~X1 + X2 + X3 + X4 + X5 + X6 + X7, group1, method = "class", parms = list(split = "information"))
-    models[[counter]] = model7
-    counter = counter + 1
-    
-    model8 <- rpart(Response~Y1 + Y2 + Y3 + Y4 + Y5 + Y6 + Y7, group1, method = "class", parms = list(split = "information"))
-    models[[counter]] = model8
-    counter = counter + 1
-    
-    model9 <- rpart(Response~X1 + X2 + X3 + X4 + X5 + X6 + X7 + Y1 + Y2 + Y3 + Y4 + Y5 + Y6 + Y7,group0, method = "class", parms = list(split = "information"))
-    models[[counter]] = model9
-  } 
-}
+pruned_models <- list()
+count <- 1
 
-#function to do multiple runs
-multiple_runs <- function(model,num_runs,dataset,prune_tree=FALSE){
-  for (i in 1:n){
-      opt <- which.min(rpart_model$cptable[,"xerror"])
-      cp <- rpart_model$cptable[opt, "CP"]
-  }
-  return(fraction_correct)
-}
-# Writing to pdf
-# Ending for loop to write the list of models to a pdf file
 pdf("models.pdf", onefile = TRUE)
-for (i in seq(length(models))) {
-  printcp(models[[i]])
-  plot(as.party(models[[i]]))
+for(i in seq(length(sub_sets))) {
+  for(j in seq(length(formulas))) {
+    model <- rpart(formula = formulas[[j]], data = sub_sets[[i]], method = "class")
+    pruned_models[count] <- prune(model, cp = cps[[count]])
+    plot(as.party(pruned_models[[count]]))
+    count <- count + 1
+  }
 }
 dev.off()
+
+# The difficult way
+
+png("modeldata_X.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[1]], data = sub_sets[[1]], method = "class")
+pruned_model <- prune(model, cp = cps[[1]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modeldata_Y.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[2]], data = sub_sets[[1]], method = "class")
+pruned_model <- prune(model, cp = cps[[2]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modeldata_XY.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[3]], data = sub_sets[[1]], method = "class")
+pruned_model <- prune(model, cp = cps[[3]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modelG0_X.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[1]], data = sub_sets[[2]], method = "class")
+pruned_model <- prune(model, cp = cps[[4]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modelG0_Y.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[2]], data = sub_sets[[2]], method = "class")
+pruned_model <- prune(model, cp = cps[[5]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modelG0_XY.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[3]], data = sub_sets[[2]], method = "class")
+pruned_model <- prune(model, cp = cps[[6]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modelG1_X.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[1]], data = sub_sets[[3]], method = "class")
+pruned_model <- prune(model, cp = cps[[7]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modelG1_Y.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[2]], data = sub_sets[[3]], method = "class")
+pruned_model <- prune(model, cp = cps[[8]])
+plot(as.party(pruned_model))
+dev.off()
+
+png("modelG1_XY.png", width = 500, height = 500)
+model <- rpart(formula = formulas[[3]], data = sub_sets[[3]], method = "class")
+pruned_model <- prune(model, cp = cps[[9]])
+plot(as.party(pruned_model))
+dev.off()
+
