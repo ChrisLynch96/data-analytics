2be675beeba44b4e3374cfee7517249daebcd2a4
ChrisLynch96 <chlynch@tcd.ie>
2019-10-31 21:11:14 +0000

    Lab 8 Q2

--- labs/lab8/Lab8Q2.R
+++ labs/lab8/Lab8Q2.R
@@ -58,6 +58,26 @@ generate_pdfs <- function(samples, lambda) {
   return(pdfs)
 }
 
+generate_pdfs <- function(samples, shape, lambda) {
+  pdfs <- list()
+  for(i in 1:length(samples)) {
+    pdfs[[i]] <- dgamma(samples[[i]],shape = shape, rate = lambda)
+  }
+  
+  return(pdfs)
+}
+
+ks_testing <- function(samples, shape, rate) {
+  results <- rep(NA, length(samples))
+  
+  for(i in 1:length(samples)) {
+    ks_result <- ks.test(samples[[i]], "pgamma", shape, rate)
+    results[i] <- ks_result$p.value
+  }
+  
+  return(results)
+}
+
 # main
 k <- 5
 lambda <- 3
@@ -69,6 +89,15 @@ gamma_samples <- gen_gamma_samples_from_exp_variables(k, lambda, num_samples, sa
 # Checking the summation of exponential samples gives a correct gamma distribution
 hist(gamma_samples[[1]])
 
+sample_pdfs <- generate_pdfs(gamma_samples, k, lambda)
 
+plot(x = gamma_samples[[1]], y = sample_pdfs[[1]], xlab = "x", ylab = "Probability density", type = "p")
 
+# kolmogorov smirnov test
+results <- ks_testing(gamma_samples, k, lambda)
+
+sprintf("Mean p value from ks test over %d samples: %f", num_samples, mean(results))
+sprintf("Variance of p value from ks test over %d samples: %f", num_samples, var(results))
+sprintf("Max p value from ks test over %d samples: %f", num_samples, max(results))
+sprintf("Min p value from ks test over %d samples: %f", num_samples, min(results))
 
