3289394e53684c37eeb63f488f3c99921c7f86aa
ChrisLynch96 <chlynch@tcd.ie>
2019-10-31 17:50:53 +0000

    Now generating the requested number of samples and verifying they work

--- labs/lab8/Lab8.R
+++ labs/lab8/Lab8.R
@@ -6,20 +6,56 @@ rm(list = ls())
 
 # functions
 
-gen_exponential_dist <- function(num_samples, lambda) {
-  exponential_distribution <- rep(NA, num_samples)
+gen_samples <- function(lambda = 1, num_samples, sample_size) {
+  samples <- list()
   
   for(i in 1:num_samples) {
-    exponential_distribution[i] <- exponential_sample(lambda)
-    print(exponential_distribution[i])
+    samples[[i]] <- gen_exponential_sample_via_cdf_inverse(lambda, sample_size)
   }
   
+  return(samples)
+}
+
+gen_exponential_sample_via_cdf_inverse <- function(lambda = 1, sample_size) {
+  exponential_distribution <- rep(NA, sample_size)
+  uniform_distribution <- runif(sample_size, 0, 1)
+  
+  for(i in 1:sample_size) {
+    exponential_distribution[i] <- cdf_inverse(lambda, uniform_distribution[i])
+  }
   return(exponential_distribution)
 }
 
-exponential_sample <- function(lambda) {
-  sample <- (-(1/lambda))*(log(1-runif(1, 0, 1))) # stick in a random value between 0 and 1
-  return(sample)
+cdf_inverse <- function(lambda, x) {
+  inverse <- (-(1/lambda))*(log(1-x))
+  return(inverse)
 }
 
-sample <- gen_exponential_dist(2000, 1)
+generate_pdfs <- function(samples, lambda) {
+  pdfs <- list()
+  for(i in 1:length(samples)) {
+    pdfs[[i]] <- dexp(samples[[i]], rate = lambda)
+  }
+  
+  return(pdfs)
+}
+
+# main
+
+set.seed(124)
+lambda <- 1
+num_samples <- 2000
+sample_size <- 2000
+
+samples <- gen_samples(lambda, num_samples, sample_size)
+
+# checking that the generated samples follow a exponential distribution
+hist(samples[[1]])
+
+sample_pdfs <- generate_pdfs(samples, lambda)
+
+# Testing the pdf of the exponetial distribution sample is correct
+plot(x = samples[[1]], y = sample_pdfs[[1]], xlab = "x", ylab = "Probability density", type = "p")
+
+
+
