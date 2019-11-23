47f5b7815d001eb4c758f0afb79b940da9d92f5f
ChrisLynch96 <chlynch@tcd.ie>
2019-11-23 17:23:47 +0000

    started to fix lab 7 should finixh during study

--- labs/lab7/lab7.R
+++ labs/lab7/lab7.R
@@ -1,1 +1,14 @@
+# Lab 7: Two stage modelling for variable imputation that can be 0 or 1
+
+## Setup
+
 rm(list=ls())
+setwd("~/workspace/github.com/ChrisLynch96/data-analytics/labs/lab7")
+
+## functions
+
+## main
+
+SIS <- 
+
+glm.sign <- glm (I(earnings>0) ~ male + over65 + white + immig + educ_r + any.ssi + any.welfare + any.charity, data=SIS, family=binomial(link=logit))
