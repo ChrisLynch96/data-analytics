cc25ee022d5d5181a7bc1e1cc73bf69f4b647987
ChrisLynch96 <chlynch@tcd.ie>
2019-11-23 16:28:37 +0000

    initial code for project 2

---
+++ assignments/assign3/assignment3.R
@@ -0,0 +1,18 @@
+# Assignmen 3: Data imputation
+
+## Setup
+
+rm(list = ls())
+setwd("~/workspace/github.com/ChrisLynch96/data-analytics/assignments/assign3")
+
+library(readxl)
+library(mice)
+library(VIM)
+
+## functions
+
+## main
+
+data <- read_excel("Project Data.xlsx")
+
+md.pattern(data)
